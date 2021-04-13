785aa9aee8694fa4153d2d5978c23b1188f0ec5d
pavel.z <pavel.zayats@geomotiv.com>
2021-04-13 17:58:50 +0300

    Update composer and emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '10.0.1',
+    'version' => '11.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.9.99',
+            'typo3' => '10.0.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
